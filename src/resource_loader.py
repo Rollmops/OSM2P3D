fff66612b77be9687644c77d139871e7fe629e3e
Erik Türke <etuerke@googlemail.com>
2014-08-06 13:38:54 +0200

    modified resources file

--- src/resource_loader.py
+++ src/resource_loader.py
@@ -8,5 +8,5 @@ def get():
     for type in resources_xml:
         ret[type.get('name')] = {}
         for node in type:
-            ret[type.get('name')][node.get("name")] = (node.get("key"), node.get("value"), [i for i in node])
+            ret[type.get('name')][node.get("name")] = (node.get("key"), node.get("value"), [(i, True) for i in node])
     return ret
